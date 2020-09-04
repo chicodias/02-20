7190902f1f196ccfabe42f607e4f07e97b742b27
Chico <chicodias@outlook.com>
2020-09-04 16:09:40 -0300

    at02_final

--- SCC0223/02-ex2/sacola2.c
+++ SCC0223/02-ex2/sacola2.c
@@ -7,26 +7,27 @@
 
 
 /* estrutura que armazena um par formado por um elemento do tpo ITEM e um inteiro que indicará o número de ocorrências */
+typedef
 struct Elemento
 {
     char valor;
     int num;
-};
+} ITEM;
+
+/* elems como um vetor de pares*/
+/* ◦variável used informa quantas posições de elems já foram uIlizadas */  
 
 struct sacola
 {
-    struct Elemento elem[TAM_MAX];
+    ITEM elem[TAM_MAX];
     int used; 
 };
-/* elems como um vetor de pares*/
-/* ◦variável used informa quantas posições de elems já foram uIlizadas */  
 
 
 /*Contrução do tipo sacola que contém:
 elem: vetor de char de TAM_MAX
 used: inteiro que armazena o número de char contidos em elem
-*/
-/*
+
 Criação da sacola com alocação dinâmica e inicialização do contador used
 Retorna o ponteiro da sacola
 */
@@ -65,17 +66,17 @@ Só insere se ainda possui espaço (verificação através do elemento used)
 */
 
 void inserir(SACOLA *sac, char c){
-    if (sac -> used >= TAM_MAX) //sacola cheia
-        return;
-
     for (int i = 0; i < sac ->used; i++) // incrementa elemento na sacola
         if(sac->elem[i].valor == c){
             sac->elem[i].num++;
-            sac->used++;
             /*printf("Elemento %c inserido\n", c);*/
             return;
         }
-    sac->elem[sac->used].valor = c;
+
+    if (sac -> used >= TAM_MAX) //sacola cheia
+        return;
+        
+    sac->elem[sac->used].valor = c; // insere elemento que nao estava na sacola ainda
     sac->elem[sac->used].num = 1;
     sac->used++;
     
