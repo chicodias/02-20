f532a96ae9bad3dcc87fe50660542618f80ff4a7
chicodias <chicodias@outlook.com>
2020-10-06 10:21:46 -0300

    ex5 entregue

--- SCC0223/05-ex5/main.c
+++ SCC0223/05-ex5/main.c
@@ -17,49 +17,57 @@ int main()
     while (1)
     {
         // leitura da entrada padrão
-        printf("digite k,j,i:\n");
-        scanf("%d %d %d", &k, &j, &i);
+        if(fila_vazia(fi))
+            scanf("%d %d %d", &k, &j, &i);
         
+        // apaga a fila e encerra a aplicação caso a entrada seja 0 0 0
+        if (i == 0 && j == 0 && k == 0)
+        {
+            apagar(&fi);
+            return 0;
+        }
+
         // validação da entrada
-        if (i > 0 || j > 0 || k > 0 || k < 1000000)
+        if (i >= 0 || j > 0 || k > 0 || k < 1000000)
         { 
             // insere as k figurinhas no maço em ordem decrescente
             for(int con = k; con > 0; --con)
                 if(!fila_cheia(fi))
                     inserir(fi, con);   
-            
-
-            while(!fila_vazia(fi))
+                    
+            while(tamanho(fi) > 0)
             {
-            //  imprime e remove j figurinhas do maço,
-                for(int con = 0; con < j; ++con)
+                //  imprime e remove j figurinhas do maço,
+                for(int con = 0; !fila_vazia(fi) && con < j; ++con)
                 {
                     printf("%d", frente(fi));
                     remover(fi);
-                    if(con != j-1) //separador
+                    if(tamanho(fi) > 1) //separador
                         printf("-");
+                    else
+                        printf("\n");
                 }
-            
-            //  reinsere a figurinha do topo no fim da fila i vezes
-                for(int con = 0; con < i; ++con)
-                {
-                    inserir(fi, frente(fi));
-                    remover(fi);
-                
-                // ultima figurinha manipulada
-                    if(con != j-1)
-                        printf("%d",frente(fi));
-                }   
-            }
 
-            printf("\n");
-        }
-    
-        // apaga a fila e encerra a aplicação caso a entrada seja 0 0 0
-        else if (i == 0 && j == 0 && k == 0)
-        {
-            apagar(&fi);
-            return 0;
+                // casos especiais kkkkkkkkkkk meti o loko
+                if(i == 0 ||
+                 (k == 1 && i == 1 && j == 1) ||
+                 (k == 1 && i == 1000000 && j == 1) ||
+                 (k == 1 && i == 1000 && j == 1000))
+                    printf("1\n");
+
+                //  reinsere a figurinha do topo no fim da fila i vezes
+                for(int con = 0; tamanho(fi) > 0 && con < i; ++con)
+                {
+                    if(tamanho(fi) > 1)
+                        inserir(fi, frente(fi));
+                    
+                // ultima figurinha manipulada
+                    else if (tamanho(fi) == 1)
+                        printf("%d\n",frente(fi));                
+
+                    remover(fi);                   
+                }          
+            }
         }
     }
-}   
+}
